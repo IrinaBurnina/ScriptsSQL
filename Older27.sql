3d96485fee1f059986af540c93f99183b3a4079c
Irina Burnina <irinkaburninka@gmail.com>
2023-10-29 17:58:52 +0300

    commit init

---
+++ Older27.sql
@@ -0,0 +1,2 @@
+select name, surname, age, phone_number, city_of_living from homework.persons
+where age>27 order by age desc;
---
+++ ScriptCreateTheTable.sql
@@ -0,0 +1,7 @@
+create table homework.PERSONS(
+name varchar(55) not null,
+surname varchar(55) not null,
+age int not null,
+phone_number varchar(55),
+city_of_living varchar(55),
+primary key( name, surname,age));
